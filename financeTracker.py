9eedd2cd4871c5d149d6de72d384567ffbe714c5
asimkhan5654 <152837375+asimkhan5654@users.noreply.github.com>
2023-12-04 00:04:45 -0500

    Update financeTracker.py

--- financeTracker.py
+++ financeTracker.py
@@ -1,1 +1,49 @@
-f
+import sqlite3
+from datetime import datetime
+
+class FinanceTracker:
+    def __init__(self, db_name='finance_tracker.db'):
+        # Connect to the SQLite database
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+
+        # Initialize tables
+        self.initialize_database()
+
+    def initialize_database(self):
+        # Create tables if not exists
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS income (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                source TEXT,
+                amount REAL,
+                date DATE
+            )
+        ''')
+        
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS expenses (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                amount REAL,
+                date DATE,
+                category TEXT
+            )
+        ''')
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS budgets (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                category TEXT,
+                limit REAL
+            )
+        ''')
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS savings_goals (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                target_amount REAL,
+                deadline DATE
+            )
+        ''')
